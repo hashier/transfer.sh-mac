4344f16d87bc0e15f42e273eefc77b52ed22ce00
Christopher Loessl <cloessl+github@gmail.com>
2016-10-14 18:13:18 +0200

    Initial Commit

---
+++ transfer.sh/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  transfer.sh
+//
+//  Created by Christopher Loessl on 2016-10-14.
+//  Copyright © 2016 chl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ transfer.sh/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  transfer.sh
+//
+//  Created by Christopher Loessl on 2016-10-14.
+//  Copyright © 2016 chl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
